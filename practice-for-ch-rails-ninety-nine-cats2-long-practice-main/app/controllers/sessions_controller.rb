f2a5a0cd0335715b6fbecaf07fd24e95bae34530
Kevin Chan <chankevin13@gmail.com>
2023-02-20 22:19:07 -0500

    userscontroller

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -0,0 +1,3 @@
+class SessionsController < ApplicationRecord
+
+end
---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationRecord
+    def new
+        @user = User.new
+
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to cats_url
+        else
+            puts @user.error.full_messages
+            render :new
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
