c29c0cabf79fd8e2d4ecf01b66373f86e66b0f08
Kevin Chan <chankevin13@gmail.com>
2023-02-20 22:24:22 -0500

    sessionsController

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationRecord
+    def new
+        render :new
+    end
 
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        @user.reset_session_token!
+        redirect_to cats_url
+    end
+
+    def destroy
+    end
 end
